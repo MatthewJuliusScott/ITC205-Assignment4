3089effe4a91a3e611a274b97bcb526a66edbe88
Matthew <matthewjuliusscott@gmail.com>
2017-10-11 15:01:32 +1100

    produced simplified automated test that repoduces bug 4

--- src/TestAssignment4.java
+++ src/TestAssignment4.java
@@ -36,5 +36,28 @@ public class TestAssignment4 {
 		String msg = "Balance %d minus bet %d did not exceed limit %d";
 		Assert.assertTrue(String.format(msg, balance, bet, limit), exceeds);
 	}
+	
+	@Test
+	public void testPlayRound_ReturnWinnings() {
+		Player player = new Player("TestPlayer", 100);
+		Integer expected = 95;
+		Dice d1 = org.mockito.Mockito.mock(Dice.class);
+		Dice d2 = org.mockito.Mockito.mock(Dice.class);
+		Dice d3 = org.mockito.Mockito.mock(Dice.class);
+		
+		when(d1.getValue()).thenReturn(DiceValue.ANCHOR);
+		when(d2.getValue()).thenReturn(DiceValue.ANCHOR);
+		when(d3.getValue()).thenReturn(DiceValue.ANCHOR);
+		
+		// assert than the player's balance doesn't increase
+		Game game = new Game(d1, d2, d3);
+		int winnings = game.playRound(player, DiceValue.CROWN, 5);
+		Integer actual = player.getBalance();
+		Assert.assertEquals(expected, actual);
+		
+		// assert that the winnings returned is 0
+		String msg = "Winnings %d should be equal to 0";
+		Assert.assertTrue(String.format(msg, winnings), winnings == 0);
+	}
 
 }
